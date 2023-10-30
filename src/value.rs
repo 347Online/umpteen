30a5387bde0dc98e7f7c45ebefe58f6968b88f47
347Online <38228372+347Online@users.noreply.github.com>
2023-10-29 22:26:40 -0500

    Improvements to value repr

--- src/value.rs
+++ src/value.rs
@@ -1,4 +1,7 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{
+    fmt::Display,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
 static COUNTER: AtomicUsize = AtomicUsize::new(0);
 
@@ -6,7 +9,7 @@ static COUNTER: AtomicUsize = AtomicUsize::new(0);
 pub enum ValueData<'d> {
     Empty,
     Boolean(bool),
-    Number(&'d f64),
+    Number(f64),
     String(&'d String),
 }
 
@@ -24,12 +27,16 @@ impl<'v> Value<'v> {
         }
     }
 
-    pub fn number(num: &'v f64) -> Self {
-        Value::new(ValueData::Number(num))
-    }
-
     pub fn is(&self, other: Self) -> bool {
         self.id == other.id
+    }
+
+    pub fn number<N: Into<f64>>(num: N) -> Self {
+        Value::new(ValueData::Number(num.into()))
+    }
+
+    pub fn string(st: &'v String) -> Self {
+        Value::new(ValueData::String(st))
     }
 }
 
@@ -39,19 +46,40 @@ impl<'v> PartialEq for Value<'v> {
     }
 }
 
+impl<'v> Display for Value<'v> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let tmp;
+
+        let repr = match self.data {
+            ValueData::Empty => "<Empty>",
+            ValueData::Boolean(x) => {
+                tmp = x.to_string();
+                &tmp
+            }
+            ValueData::Number(x) => {
+                tmp = x.to_string();
+                &tmp
+            }
+            ValueData::String(x) => x,
+        };
+
+        write!(f, "{repr}")
+    }
+}
+
 #[test]
 fn test_value_cmp() {
     let num = 100.0;
-    let v1 = Value::number(&num);
-    let v2 = Value::number(&num);
+    let v1 = Value::number(num);
+    let v2 = Value::number(num);
 
     assert_eq!(v1, v2);
     assert!(!v2.is(v1));
     assert!(v1.is(v1));
 
     let num = f64::NAN;
-    let v1 = Value::number(&num);
-    let v2 = Value::number(&num);
+    let v1 = Value::number(num);
+    let v2 = Value::number(num);
 
     assert_ne!(v1, v2);
     assert!(!v2.is(v1));
