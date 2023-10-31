94d3862d16e4e0c8174b4c883842cd5e53f70942
347Online <38228372+347Online@users.noreply.github.com>
2023-10-31 00:09:03 -0500

    Better error repr

--- src/error.rs
+++ src/error.rs
@@ -1,78 +1,37 @@
 use std::fmt::Display;
 
+use crate::instr::Instruction;
+
 pub type UmpResult<T> = Result<T, UmpError>;
 
 #[derive(Debug)]
-pub enum UmpErrorType {
-    Unknown,
+pub enum UmpError {
+    UnexpectedToken(char),
     UnexpectedEof,
     InvalidInstruction(u8),
-    WrongNumberArguments(usize, usize),
+    WrongNumberBytes(usize, usize, Instruction),
     MissingValue(usize, u8)
-}
-
-impl Display for UmpErrorType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let tmp;
-        let desc = match self {
-            Self::Unknown => "Unknown error",
-            Self::UnexpectedEof => "Unexpected end of file",
-            Self::InvalidInstruction(byte) => {
-                tmp = format!("Invalid Instruction `{byte}`");
-                &tmp
-            }
-            Self::WrongNumberArguments(exp, got) => {
-                tmp = format!("Wrong number of arguments, expected {exp} but got {got}");
-                &tmp
-            }
-            Self::MissingValue(pos, addr) => {
-                tmp = format!("Missing value in chunk {pos} @ {addr}");
-                &tmp
-            }
-        };
-
-        write!(f, "{}", desc)
-    }
-}
-
-#[derive(Debug)]
-pub struct UmpError {
-    kind: UmpErrorType,
-    line: usize,
-}
-
-impl UmpError {
-    pub fn new(kind: UmpErrorType, line: usize) -> Self {
-        Self { kind, line }
-    }
-
-    pub fn unknown(line: usize) -> Self {
-        Self::new(UmpErrorType::Unknown, line)
-    }
-
-    pub fn invalid_instruction(byte: u8) -> Self {
-        Self::new(UmpErrorType::InvalidInstruction(byte), 0)
-    }
-
-    pub fn wrong_num_args(exp: usize, got: usize) -> Self {
-        Self::new(UmpErrorType::WrongNumberArguments(exp, got), 0)
-    }
-
-    pub fn missing_value(pos: usize, addr: u8) -> Self {
-        Self::new(UmpErrorType::MissingValue(pos, addr), 0)
-    }
 }
 
 impl Display for UmpError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.line == 0 {
-            write!(f, "Error encountered: {}", self.kind)
-        } else {
-            write!(
-                f,
-                "Error encountered on line [{}]: {}",
-                self.line, self.kind
-            )
+        match self {
+            Self::UnexpectedToken(c) => write!(f, ""),
+            Self::UnexpectedEof => write!(f, "unexpected end of file"),
+            Self::InvalidInstruction(byte) => write!(f, "invalid Instruction `{byte}`"),
+            Self::WrongNumberBytes(exp, got, instr) => 
+                write!(f, "wrong number of byte arguments for instruction {instr}, expected {exp} but got {got}"),
+            Self::MissingValue(pos, addr) => 
+                write!(f, "missing value in chunk {pos} @ {addr}")
+            
         }
     }
+}
+
+pub fn report(e: UmpError) {
+    eprintln!("ERR: {e}");
+}
+
+pub fn report_line(e: UmpError, line: usize) {
+    eprintln!("ERR: {e} on line {line}");
 }
