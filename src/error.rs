c09c081386344766b38f8ceb2215db577eb5577a
347Online <38228372+347Online@users.noreply.github.com>
2023-10-31 00:29:57 -0500

    Fix a weird problem with autoformatting

--- src/error.rs
+++ src/error.rs
@@ -10,21 +10,27 @@ pub enum UmpError {
     UnexpectedEof,
     InvalidInstruction(u8),
     WrongNumberBytes(usize, usize, Instruction),
-    MissingValue(usize, u8)
+    WrongNumberArguments(usize, usize, String),
+    MissingValue(usize, u8),
 }
 
 impl Display for UmpError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::UnexpectedToken(c) => write!(f, ""),
-            Self::UnexpectedEof => write!(f, "unexpected end of file"),
-            Self::InvalidInstruction(byte) => write!(f, "invalid Instruction `{byte}`"),
-            Self::WrongNumberBytes(exp, got, instr) => 
-                write!(f, "wrong number of byte arguments for instruction {instr}, expected {exp} but got {got}"),
-            Self::MissingValue(pos, addr) => 
-                write!(f, "missing value in chunk {pos} @ {addr}")
-            
-        }
+        let desc = match self {
+            Self::UnexpectedToken(c) => format!("unexpected token `{}`", c),
+            Self::UnexpectedEof => "unexpected end of file".to_string(),
+            Self::InvalidInstruction(byte) => format!("invalid Instruction `{}`", byte),
+            Self::WrongNumberBytes(exp, got, instr) => format!(
+                "wrong number of byte arguments for instruction {}, expected {} but got {}",
+                instr, exp, got
+            ),
+            Self::WrongNumberArguments(exp, got, call) => format!(
+                "wrong number of arguments for {}, expected {} but got {}",
+                call, exp, got
+            ),
+            Self::MissingValue(pos, addr) => format!("missing value in chunk {} @ {}", pos, addr),
+        };
+        write!(f, "{desc}")
     }
 }
 
