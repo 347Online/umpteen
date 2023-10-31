c8a7730438430ba95d45527beadd43a425a152c0
347Online <38228372+347Online@users.noreply.github.com>
2023-10-31 09:05:48 -0500

    Adjusting the calling convention for the main run function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use umpteen::{Result, Umpteen};
 
 fn main() -> Result<()> {
-    Umpteen::exec("let x = 10;")
+    Umpteen::run("let x = 10;")
 }
 
 #[cfg(test)]
@@ -10,17 +10,17 @@ mod tests {
 
     #[test]
     fn test_hello_world() -> umpteen::Result<()> {
-        Umpteen::exec("print 10;")
+        Umpteen::run("print 10;")
     }
 
     #[test]
     fn test_hand_assembled_chunk() -> umpteen::Result<()> {
         let mut chunk = Chunk::new();
         let constant = chunk.write_val(Value::Number(10.7));
-        chunk.write_instr(Instruction::Constant);
-        chunk.write_byte(constant);
+        chunk.write_instr(Instruction::U8);
+        chunk.write_byte(constant as u8);
         chunk.write_instr(Instruction::Print);
         println!("{:?}", chunk);
-        Umpteen::run(vec![chunk])
+        chunk.exec()
     }
 }
