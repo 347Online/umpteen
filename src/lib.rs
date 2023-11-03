5ba34cda114d3cec50a0f5b3e763b18e504943ea
347Online <38228372+347Online@users.noreply.github.com>
2023-11-02 19:24:19 -0500

    Basic convention for invoking the runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,27 @@
-use repr::error::Error;
+use exec::{lexer::Lexer, vm::Vm};
+use repr::{error::Error, value::Value};
 
 pub mod exec;
 pub mod repr;
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Default)]
+pub struct Runtime {
+    compiler: Compiler,
+    vm: Vm,
+}
+
+impl Runtime {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn run(&mut self, src: &str) -> Result<Value> {
+        let tokens = Lexer::new(src).scan();
+        // todo!
+        let ast = Parser::new(tokens).parse()?;
+        let bytecode = self.compiler.compile(ast);
+        vm.exec(bytecode)
+    }
+}
