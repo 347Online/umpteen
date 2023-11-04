9b0ae683e0e0c9713a4edc35d9db280a5917c1a0
347Online <38228372+347Online@users.noreply.github.com>
2023-11-04 09:27:45 -0500

    Comment out runtime for now

--- src/umpteen/runtime.rs
+++ src/umpteen/runtime.rs
@@ -1,35 +1,34 @@
-use crate::{bytecode::Chunk, value::Value, Result};
+// use crate::{ value::Value, Result, bytecode::Program};
 
-pub type Program = Vec<Chunk>;
-pub type Stack = Vec<Value>;
+// pub type Stack = Vec<Value>;
 
-#[derive(Default)]
-pub struct Runtime {
-    stack: Stack,
-    program: Program,
-    index: usize,
-}
+// #[derive(Default)]
+// pub struct Runtime {
+//     stack: Stack,
+//     program: Program,
+//     index: usize,
+// }
 
-impl Runtime {
-    pub fn new(program: Program) -> Self {
-        Runtime {
-            stack: vec![],
-            program,
-            index: 0,
-        }
-    }
+// impl Runtime {
+//     pub fn new(program: Program) -> Self {
+//         Runtime {
+//             stack: vec![],
+//             program,
+//             index: 0,
+//         }
+//     }
 
-    pub fn exec(mut self, stack: &mut Stack) -> Result<Value> {
-        let mut program = std::mem::take(&mut self.program).into_iter();
+//     pub fn exec(mut self, stack: &mut Stack) -> Result<Value> {
+//         let mut program = std::mem::take(&mut self.program).into_iter();
 
-        let value = loop {
-            if let Some(chunk) = program.next() {
-                chunk.exec(stack)?;
-            } else {
-                break Value::Empty;
-            }
-        };
+//         let value = loop {
+//             if let Some(chunk) = program.next() {
+//                 chunk.exec(stack)?;
+//             } else {
+//                 break Value::Empty;
+//             }
+//         };
 
-        Ok(value)
-    }
-}
+//         Ok(value)
+//     }
+// }
