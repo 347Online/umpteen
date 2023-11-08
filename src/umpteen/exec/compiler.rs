f68975dd3c77dec28991a9c7d7be8e835c15b578
347Online <38228372+347Online@users.noreply.github.com>
2023-11-07 22:33:16 -0600

    Compiler largely rewritten, runtime is next

--- src/umpteen/exec/compiler.rs
+++ src/umpteen/exec/compiler.rs
@@ -1,47 +1,167 @@
-use crate::repr::bytecode::{chunk::Chunk, instruction::Instr};
+use crate::{
+    error::CompilerError,
+    repr::{
+        ast::{expr::Expr, stmt::Stmt},
+        bytecode::{chunk::Chunk, instruction::Instr},
+        value::Value,
+    },
+};
 
-use super::{env::Memory, parse::Ast};
+use super::parse::Ast;
 
+#[derive(Debug, Default)]
+pub struct Program {
+    chunks: Vec<Chunk>,
+    v_table: Vec<Value>,
+}
+
+impl Program {
+    pub fn write_chunk(&mut self, chunk: Chunk) {
+        self.chunks.push(chunk);
+    }
+
+    pub fn write_value(&mut self, value: Value) {
+        self.v_table.push(value);
+    }
+
+    pub fn v_table(&self) -> usize {
+        self.v_table.len()
+    }
+}
+
+#[derive(Debug, Default)]
 pub struct Compiler<'c> {
-    mem: Option<&'c Memory<'c>>,
-    program: Vec<Chunk>,
+    program: Program,
 
     instr_buf: Vec<Instr>,
-    data_buf: Vec<u8>,
+    name_buf: Vec<&'c str>,
 }
 
 impl<'c> Compiler<'c> {
     pub fn new() -> Self {
         Compiler {
-            mem: None,
-            program: vec![],
+            program: Program::default(),
 
             instr_buf: vec![],
-            data_buf: vec![],
+            name_buf: vec![],
         }
     }
 
-    pub fn compile(mut self, ast: Ast, mem: &'c mut Memory) -> Vec<Chunk> {
-        self.mem_return(mem);
-
+    pub fn compile(mut self, ast: Ast<'c>) -> Result<Program, CompilerError> {
         for stmt in ast {
-            // self.statement(stmt);
+            self.statement(stmt)?;
         }
 
-        // self.flush();
+        self.flush();
 
-        self.take_program()
+        dbg!(&self);
+
+        Ok(self.program)
     }
 
-    fn mem_take(&mut self) -> &'c mut Memory {
-        self.mem.as_mut().take().expect("Compiler memory missing")
+    fn statement(&mut self, stmt: Stmt<'c>) -> Result<(), CompilerError> {
+        match stmt {
+            Stmt::Expr(expr) => self.expression(expr)?,
+            Stmt::Print(expr) => {
+                self.expression(expr)?;
+                self.push_instr(Instr::Print);
+            }
+            Stmt::Exit => self.push_instr(Instr::Exit),
+            Stmt::Declare(name, maybe_expr) => {
+                self.push_instr(Instr::Let);
+                self.push_name(name);
+
+                if let Some(expr) = maybe_expr {
+                    self.push_instr(Instr::Set);
+                    self.push_name(name);
+                    self.expression(expr)?;
+                }
+            }
+        }
+
+        Ok(())
     }
 
-    fn mem_return(&mut self, mem: &'c mut Memory) {
-        self.mem = Some(mem);
+    fn expression(&mut self, expr: Expr<'c>) -> Result<(), CompilerError> {
+        match expr {
+            Expr::Value(value) => {
+                self.push_instr(Instr::Push);
+                self.push_value(value);
+            }
+            Expr::Assign { target, expr } => {
+                self.push_instr(Instr::Set);
+                self.expression(*target)?;
+                self.expression(*expr)?;
+            }
+            Expr::Ident { name } => {
+                self.push_instr(Instr::Get);
+                self.push_name(name);
+            }
+            Expr::UnOp { expr, op } => todo!(),
+            Expr::BinOp { left, right, op } => todo!(),
+        }
+
+        Ok(())
     }
 
-    fn take_program(&mut self) -> Vec<Chunk> {
-        std::mem::take(&mut self.program)
+    fn push_instr(&mut self, instr: Instr) {
+        self.instr_buf.push(instr);
+    }
+
+    fn push_name(&mut self, name: &'c str) {
+        self.name_buf.push(name);
+    }
+
+    fn push_value(&mut self, value: Value) {
+        self.program.v_table.push(value);
+    }
+
+    fn flush(&mut self) {
+        let instr_buf = std::mem::take(&mut self.instr_buf);
+        let name_buf = std::mem::take(&mut self.name_buf);
+
+        let mut name_pos = 0;
+        let mut value_pos = 0;
+
+        macro_rules! name {
+            () => {{
+                let name = name_buf[name_pos];
+                name_pos += 1;
+                name
+            }};
+        }
+
+        let mut chunk = Chunk::new();
+
+        for instr in instr_buf {
+            chunk.write_instr(instr);
+
+            match instr {
+                Instr::Let => {
+                    let name = name_buf[name_pos];
+                    name_pos += 1;
+
+                    chunk.write_bytes(name.as_bytes());
+                }
+                Instr::Push => {
+                    chunk.write_byte(value_pos);
+                    value_pos += 1;
+                }
+                Instr::Set => {
+                    name_pos += 1;
+                    value_pos += 1;
+                }
+                Instr::Get => {
+                    let name = name_buf[name_pos];
+                    name_pos += 1;
+
+                    chunk.write_bytes(name.as_bytes());
+                }
+
+                _ => (),
+            }
+        }
+
+        self.program.write_chunk(chunk);
     }
 }
